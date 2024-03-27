38968bd785b73ad1911d7b99db745383ce2a510b
Your Name <you@example.com>
2024-03-27 08:42:53 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+
+import React from 'react'
+
+    function page()
+     {
+      return (
+        
+        <div className='bg-blue-950 text-white min-h-screen'>
+          <div className=' bg-red-500 w-[90%] mx-auto flex items-center justify-between'>
+            <h2 className='text[20px]'>Next js</h2>
+            <button className='bg-blue-300 py-2 px-7 rounded-md hover:bg-blue-500'>CV</button>
+          </div>
+</div>
+     
+  )
+      }
+
+  export default page
+
+
+
